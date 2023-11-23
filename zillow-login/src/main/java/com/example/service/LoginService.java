52ec07b379280ba64a821fa1b66004814e65e61e
sheevpalpatine <emperorsheevpalpatine0@gmail.com>
2023-11-23 19:25:47 +0800

    Merge remote-tracking branch 'origin/master'

--- zillow-login/src/main/java/com/example/service/LoginService.java
+++
@@ -1,9 +0,0 @@
-package com.example.service;
-
-import com.example.vo.ZillowResult;
-
-public interface LoginService {
-    ZillowResult sendVerificationCode(String phone);
-
-    ZillowResult login(String phone, String verificationCode);
-}
--- zillow-search/src/main/java/com/example/service/SearchService.java
+++
@@ -1,8 +0,0 @@
-package com.example.service;
-
-import com.example.vo.ZillowResult;
-
-public interface SearchService {
-    ZillowResult search(String city, String content,int page,int rows);
-    void ESInit();
-}
