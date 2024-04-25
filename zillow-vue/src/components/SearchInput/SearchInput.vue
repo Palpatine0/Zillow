d98579e127ad3946c69802efad7887f2cfe4459d
Percival <emperorsheevpalpatine0@gmail.com>
2024-04-25 19:23:35 +0800

    style: resolved the format inconsistency in index in mobile view

--- zillow-vue/src/components/SearchInput/SearchInput.vue
+++ zillow-vue/src/components/SearchInput/SearchInput.vue
@@ -1,56 +1,52 @@
 <!--SearchInput.vue-->
 <template>
-  <input v-model="msg" class='search-input' placeholder="Address, neighborhood, city, Zip" type="text"
-         @keyup="search($event)">
+    <input v-model="msg" class='search-input' placeholder="Address, neighborhood, city, Zip" type="text" @keyup="search($event)">
 </template>
 <script>
 export default {
-  name: 'SearchInput',
-  inject: ['reload'],
-  data() {
-    return {
-      msg: ''
-    }
-  },
-  props: ['pk'],
-  methods: {
-    search(e) {
-      if (e.keyCode === 13) {
-        this.$router.push('/search/' + this.msg)
-        if (this.msg == this.pk) {
-          return
+    name: 'SearchInput',
+    inject: ['reload'],
+    data() {
+        return {
+            msg: ''
         }
-        this.$emit('setSearchMsgAciton', {data: this.msg})
-      }
-    }
-  },
-  mounted() {
-    this.msg = this.pk
-    this.$emit('setSearchMsgAciton', {data: this.pk})
-  },
-  watch: {
-    'pk': function (n, o) {
-      if (n !== this.msg) {
-        this.msg = n;
-        this.$emit('setSearchMsgAciton', {data: n})
-      }
-    }
-  },
+    },
+    props: ['pk'],
+    methods: {
+        search(e) {
+            if (e.keyCode === 13) {
+                this.$router.push('/search/' + this.msg)
+                if (this.msg == this.pk) {
+                    return
+                }
+                this.$emit('setSearchMsgAciton', {data: this.msg})
+            }
+        }
+    },
+    mounted() {
+        this.msg = this.pk
+        this.$emit('setSearchMsgAciton', {data: this.pk})
+    },
+    watch: {
+        'pk': function (n, o) {
+            if (n !== this.msg) {
+                this.msg = n;
+                this.$emit('setSearchMsgAciton', {data: n})
+            }
+        }
+    },
 }
 </script>
 <style lang="less" scoped>
 .search-input {
-  //background-color: #156FF6;
-  font-size: 22px !important;
-  font-weight: normal;
-  border: 0;
-  width: 82% !important;
-  height: 22px;
-  color: #333 !important;
-  padding-left: 5px;
-  position: relative;
-  top: 25%;
-  left: 5%;
-  font-family: "Open Sans", "Adjusted Arial", Tahoma, Geneva, sans-serif !important;
+    font-size: 22px !important;
+    font-weight: normal;
+    border: 0;
+    width: 82% !important;
+    height: 22px;
+    color: #333 !important;
+    padding-left: 5px;
+    position: relative;
+    font-family: "Open Sans", "Adjusted Arial", Tahoma, Geneva, sans-serif !important;
 }
 </style>
--- zillow-vue/src/views/Home/Homehot/Homehot.vue
+++ zillow-vue/src/views/Home/Homehot/Homehot.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>
-    <HomeHotView v-if="recommendation.length>0" :hotData="recommendation" title="Recommendations"/>
-    <div v-else class="test">Loading Data...</div>
-    <HomeHotView v-if="trendy.length>0" :hotData="trendy" title="Trendies"/>
-    <div v-else class="test">Loading Data...</div>
-  </div>
+    <div>
+        <HomeHotView v-if="recommendation.length>0" :hotData="recommendation" title="Recommendations"/>
+        <div v-else class="test">Loading Data...</div>
+        <HomeHotView v-if="trendy.length>0" :hotData="trendy" title="Trendies"/>
+        <div v-else class="test">Loading Data...</div>
+    </div>
 </template>
 
 <script>
@@ -12,42 +12,44 @@ import {mapState} from 'vuex'
 import HomeHotView from "../HomeHotView/HomeHotView";
 
 export default {
-  name: "Homehot",
-  data() {
-    return {
-      trendy: [],
-      recommendation: []
-    };
-  },
-  computed: {
-    ...mapState(['curcity'])
-  },
-  components: {
-    HomeHotView
-  },
-  mounted() {
-    this.$api.hotProduct({
-      city: this.curcity
-    })
-        .then(data => {
-          if (data.data.status) {
-            this.trendy = data.data.data
-          }
+    name: "Homehot",
+    data() {
+        return {
+            trendy: [],
+            recommendation: []
+        };
+    },
+    computed: {
+        ...mapState(['curcity'])
+    },
+    components: {
+        HomeHotView
+    },
+    mounted() {
+        this.$api.hotProduct({
+            city: this.curcity
         })
-    this.$api.recommendation({
-      city: this.curcity
-    })
-        .then(data => {
-          if (data.data.status) {
-            this.recommendation = data.data.data
-          }
+                .then(data => {
+                    if (data.data.status) {
+                        this.trendy = data.data.data
+                    }
+                })
+        this.$api.recommendation({
+            city: this.curcity
         })
-  }
+                .then(data => {
+                    if (data.data.status) {
+                        this.recommendation = data.data.data
+                    }
+                })
+    }
 };
 </script>
 
 <style>
 .test {
-  font-family: Arial;
+    font-family: Arial;
 }
+
+
 </style>
