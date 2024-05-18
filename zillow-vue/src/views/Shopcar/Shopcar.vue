61b89c2a99598a32bad01a86e15e383bf57d225e
Percival <emperorsheevpalpatine0@gmail.com>
2024-05-18 09:15:06 +0800

    style: Update page mine

--- zillow-vue/src/views/Shopcar/Shopcar.vue
+++ zillow-vue/src/views/Shopcar/Shopcar.vue
@@ -2,26 +2,20 @@
     <div>
         <Header title="Mine"/>
 
-        <div class="userinfo-container">
-            <p>
-                <svg height="20" viewBox="0 0 512 512" width="20" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M399 384.2C376.9 345.8 335.4 320 288 320H224c-47.4 0-88.9 25.8-111 64.2c35.2 39.2 86.2 63.8 143 63.8s107.8-24.7 143-63.8zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 16a72 72 0 1 0 0-144 72 72 0 1 0 0 144z"
-                          fill="#1470f5"/>
-                </svg>
-                <br>
-                <h3>{{ username }}</h3>
-            </p>
-            <p>
-                <svg height="16" viewBox="0 0 384 512" width="12" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M384 192c0 87.4-117 243-168.3 307.2c-12.3 15.3-35.1 15.3-47.4 0C117 435 0 279.4 0 192C0 86 86 0 192 0S384 86 384 192z"
-                          fill="#1470f5"/>
-                </svg>
-                <br>
-                <h3>{{ cur_city }}</h3>
-            </p>
+        <div class="userinfo-container mt-3">
+            <div style="display: flex">
+                <img src="../../../src/assets/images/user.png" style="width: 40px;height: 40px;">
+                <h3 class="mt-2" style="margin-left: 10px">{{ username }}</h3>
+            </div>
+            <div class="mt-2" style="display: flex" >
+                <img src="../../../src/assets/images/location.png" style="width: 40px;height: 40px;">
+                <h3 class="mt-2" style="margin-left: 10px">{{ cur_city }}</h3>
+            </div>
         </div>
 
         <ShopCarOrder/>
+
+
         <FootNav/>
     </div>
 </template>
@@ -80,7 +74,7 @@ export default {
 .userinfo-container {
     background-color: #fff;
     padding: 10px;
-
+    width: 80%;
     position: relative;
     left: 5%;
 
