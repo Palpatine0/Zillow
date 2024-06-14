b263664ddda33df4a7846ae079f8825f141217dc
Percival <emperorsheevpalpatine0@gmail.com>
2024-06-15 03:25:53 +0800

    refactor: BE> Update naming for Eureka and Gateway

--- zillow-config-server/src/main/java/com/example/ConfigServerApplication.java
+++ zillow-config-server/src/main/java/com/example/ConfigServerApplication.java
@@ -7,8 +7,8 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class ConfigApplication {
+public class ConfigServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigApplication.class,args);
+        SpringApplication.run(ConfigServerApplication.class,args);
     }
 }
--- zillow-gateway-server/src/main/java/com/example/GatewayServerApplication.java
+++ zillow-gateway-server/src/main/java/com/example/GatewayServerApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GatewayApplication {
+public class GatewayServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(GatewayApplication.class,args);
+        SpringApplication.run(GatewayServerApplication.class,args);
     }
 }
