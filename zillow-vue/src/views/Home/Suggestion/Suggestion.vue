b4bd527053ec0b3c7e4c487ae982eb4e77f24cfa
Percival <emperorsheevpalpatine0@gmail.com>
2024-11-18 02:39:54 +0800

    refactor(FE-client>suggestion): code restructuring

--- zillow-vue/src/views/Home/Suggestion/Suggestion.vue
+++ zillow-vue/src/views/Home/Suggestion/Suggestion.vue
@@ -1,12 +1,13 @@
-<script setup>
+<template>
+<div class="mt-8">
+    <Loan/>
+    <Advise/>
+</div>
+</template>
 
+
+<script setup>
 import Loan from "@/views/Home/Suggestion/Suggestions/Loan.vue";
 import Advise from "@/views/Home/Suggestion/Suggestions/Advise.vue";
 </script>
 
-<template>
-    <div>
-        <Loan/>
-        <Advise/>
-    </div>
-</template>
