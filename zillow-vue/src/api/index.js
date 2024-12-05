85e4df2cd18a046c2f02d8b54e462c343f313d76
Percival <emperorsheevpalpatine0@gmail.com>
2024-12-05 19:05:42 +0800

    refactor(FE-client): modularized API endpoints for cleaner structure

--- zillow-vue/src/api/index.js
+++ zillow-vue/src/api/index.js
@@ -8,75 +8,94 @@ const api = {
 
 
     // file
-    getBanners() {
-        return axios.get(base.baseUrl + base.getBanners)
-    },
-    uploadFile(file) {
-        return axios.post(base.baseUrl + base.uploadFile, file, {
-            headers: {
-                'Content-Type': 'multipart/form-data'
-            }
-        })
+    file: {
+        getBanners() {
+            return axios.get(base.baseUrl + base.file.getBanners)
+        },
+        uploadFile(file) {
+            return axios.post(base.baseUrl + base.file.uploadFile, file, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            })
+        },
     },
 
-    //
-    getRecommendations(params) {
-        return axios.get(base.baseUrl + base.getRecommendations + "?city=" + params.city);
+    // trendy
+    trendy: {
+        getTrendies(params) {
+            return axios.get(base.baseUrl + base.trendy.getTrendies + "?city=" + params.city);
+        },
     },
-    getTrendies(params) {
-        return axios.get(base.baseUrl + base.getTrendies + "?city=" + params.city);
+
+    // recommendation
+    recommendation: {
+        getRecommendations(params) {
+            return axios.get(base.baseUrl + base.recommendation.getRecommendations + "?city=" + params.city);
+        },
     },
 
     // search
-    searchByKeyWord(params) {
-        return axios.get(base.baseUrl + base.searchByKeyWord + "?" + qs.stringify(params));
+    search: {
+        searchByKeyWord(params) {
+            return axios.get(base.baseUrl + base.search.searchByKeyWord + "?" + qs.stringify(params));
+        },
+        searchByCity(params) {
+            return axios.get(base.baseUrl + base.search.searchByCity + "?" + qs.stringify(params));
+        },
     },
-    searchByCity(params) {
-        return axios.get(base.baseUrl + base.searchByCity + "?" + qs.stringify(params));
-    },
-
 
     // buyAction
-    buyAction(params) {
-        return axios.get(base.baseUrl + base.buyAction + "?" + qs.stringify(params));
+    buyAction: {
+        buyAction(params) {
+            return axios.get(base.baseUrl + base.buyAction.buyAction + "?" + qs.stringify(params));
+        },
     },
 
     // item
-    getItemById(params) {
-        return axios.get(base.baseUrl + base.getItemById + "?" + qs.stringify(params));
+    item: {
+        getItemById(params) {
+            return axios.get(base.baseUrl + base.item.getItemById + "?" + qs.stringify(params));
+        },
     },
-
 
     // comment
-    addComment(params) {
-        return axios.post(base.baseUrl + base.addComment + "?" + qs.stringify(params));
+    comment: {
+        addComment(params) {
+            return axios.post(base.baseUrl + base.comment.addComment + "?" + qs.stringify(params));
+        },
+        getCommentsByItemId(params) {
+            return axios.get(base.baseUrl + base.comment.getCommentsByItemId + "?" + qs.stringify(params));
+        },
     },
-    getCommentsByItemId(params) {
-        return axios.get(base.baseUrl + base.getCommentsByItemId + "?" + qs.stringify(params));
-    },
-
 
     // user
-    login(params) {
-        return axios.post(base.baseUrl + base.login, qs.stringify(params));
-    },
-    register(params) {
-        return axios.post(base.baseUrl + base.register, qs.stringify(params));
-    },
-    getUserByUsername(params) {
-        return axios.get(base.baseUrl + base.getUserByUsername + "?" + qs.stringify(params));
+    user: {
+        login(params) {
+            return axios.post(base.baseUrl + base.user.login, qs.stringify(params));
+        },
+        register(params) {
+            return axios.post(base.baseUrl + base.user.register, qs.stringify(params));
+        },
+        getUserByUsername(params) {
+            return axios.get(base.baseUrl + base.user.getUserByUsername + "?" + qs.stringify(params));
+        },
     },
 
+
     // order
-    getOrdersByUserId(params) {
-        return axios.get(base.baseUrl + base.getOrdersByUserId + "?" + qs.stringify(params));
+    order: {
+        getOrdersByUserId(params) {
+            return axios.get(base.baseUrl + base.order.getOrdersByUserId + "?" + qs.stringify(params));
+        },
     },
 
     // llm
-    chat(params){
-        return axios.get(base.baseUrl + base.chat + "?" + qs.stringify(params));
+    llm: {
+        chat(params) {
+            return axios.get(base.baseUrl + base.llm.chat + "?" + qs.stringify(params));
+        }
     }
-
 }
 
 export default api;
