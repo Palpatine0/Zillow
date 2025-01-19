426ecc8d5236f6f5b1ff4225c944f9a557a6160e
Percival <emperorsheevpalpatine0@gmail.com>
2025-01-20 00:49:14 +0800

    config: updated configuration to use domain name ‘percival.services’ instead of IP address

--- zillow-config-server/src/main/java/com/example/ConfigServerApplication.java
+++ zillow-config-server/src/main/java/com/example/ConfigServerApplication.java
@@ -9,6 +9,6 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 public class ConfigServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServerApplication.class,args);
+        SpringApplication.run(ConfigServerApplication.class, args);
     }
 }
